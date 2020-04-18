5298289367f63483c1738da36c7fee4a429b58a5
costar011 <51503128+costar011@users.noreply.github.com>
2020-04-18 15:37:13 +0900

    create Appbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { HashRouter as Router, Route } from "react-router-dom";
+import AppShell from "./AppBar";
+import TextScreen from "./TextScreen";
+import FileScreen from "./FileScreen";
+import WordCloudeScreen from "./WordCloudScreen";
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <div>Hello React</div>
-      </div>
+      <Router>
+        <AppShell>
+          <div>
+            <Route exact path="/" component={TextScreen} />
+            <Route exact path="/file" component={FileScreen} />
+            <Route exact path="/wc" component={WordCloudeScreen} />
+          </div>
+        </AppShell>
+      </Router>
     );
   }
 }
